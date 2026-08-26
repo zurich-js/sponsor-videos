28dffa9260a726271c10b5298dd1bf1c910ad28f
Bogdan Ilie <ilie06@gmail.com>
2026-08-26 10:20:56 +0200

    fix: extend Google Ads video to 10 seconds

--- src/Root.tsx
+++ src/Root.tsx
@@ -12,7 +12,7 @@ export const RemotionRoot: React.FC = () => {
       <Composition
         id="ZurichJsGoogleAds2026"
         component={GoogleAdsCampaign}
-        durationInFrames={240}
+        durationInFrames={300}
         fps={30}
         width={1920}
         height={1080}
